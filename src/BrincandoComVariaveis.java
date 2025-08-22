6e415e9cf188b77b08d8119d1fb9a30ae59732ba
Julio C. Jesus <143628023+juliocesarcj@users.noreply.github.com>
2025-08-22 04:58:41 +0000

    Novo novo Banco/novo arquivo:variaveis

---
+++ src/BrincandoComVariaveis.java
@@ -0,0 +1,12 @@
+public class brincandoComVariaveis {
+    public static void main(String[] args) {
+        int caixaDaSala = 15;
+        int caixaDoQuarto = 45;
+        int caixaDoBanheiro = 10;
+        System.out.println(caixaDaSala);
+        System.out.println(caixaDoQuarto);
+        System.out.println(caixaDoBanheiro);
+
+
+    }
+}
