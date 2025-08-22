10fa03d6f0cca8dc7e9e897c00ed919fcc767bc0
juliocesarcj <juliojesus318@gmail.com>
2025-08-22 02:27:15 +0000

    carteira de identificacao

---
+++ src/id/src/id.java
@@ -0,0 +1,15 @@
+public class id {
+    public static void main(String[] args) {
+        String name = "Juilio";
+        String state = "DEV";
+        char gender = 'M';
+        int age = 23;
+        boolean organDonor = true;
+
+        System.out.printf("nome: %s%n", name);
+        System.out.printf("status: %s%n",state);
+        System.out.printf("sexo: %c%n",gender);
+        System.out.printf("idade: %d%n",age);
+        System.out.printf("doador: %b%n",organDonor);
+    }
+}
