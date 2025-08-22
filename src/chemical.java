72de802264404188c9cf3519fb23a410f5ea219e
juliocesarcj <juliojesus318@gmail.com>
2025-08-22 02:43:59 +0000

    Mix chemical

---
+++ src/chemical.java
@@ -0,0 +1,7 @@
+public class chemical {
+    public static void main(String[] args) {
+        double a=3.333, b=4.65;
+        double reactionResult = (a+b)/(a*b);
+        System.out.println(reactionResult);
+    }
+}
